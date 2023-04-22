ab9635550602544c962669977149308a3f325da4
Elie Hatem <e.na.hatem@gmail.com>
2023-04-22 17:15:34 +0200

    Modified day 14 project code

--- day_14/main.py
+++ day_14/main.py
@@ -3,72 +3,62 @@ from os import system
 import art
 from game_data import data
 
+def clear_screen():
+    system('cls')
+    print(art.logo)
 
+def check_answer(guess, follower_count_a, follower_count_b):
+    if follower_count_a > follower_count_b:
+        return guess == 'a'
+    else:
+        return guess == 'b'
 
 def higher_lower():
 
+    # Printing art logo
     print(art.logo)
 
     # Get data list size
     data_size = len(data)
 
-    # TODO: Generate 2 random numbers
+    # Generate 2 random numbers
     random_number_1 = random.randint(0, data_size-1)
     random_number_2 = random.randint(0, data_size-1)
 
     while random_number_1 == random_number_2:
         random_number_2 = random.randint(0, data_size-1)
 
-    # TODO: Create score for user
+    # Create score for user
     score = 0
 
-    # TODO: Output options on the screen and ask user to pick between them
+    # Output options on the screen and ask user to pick between them
     option_a = data[random_number_1]
     option_b = data[random_number_2]
 
     should_continue = True
+    # Create while loop to repeat the program until the user chooses wrong
     while should_continue:
-        # TODO: Create while loop to repeat the program until the user chooses wrong
-
         print(f"Compare A: {option_a['name']}, a {option_a['description']}, from {option_a['country']}")
         print(art.vs)
-        print(f"Compare B: {option_b['name']}, a {option_b['description']}, from {option_b['country']}")
-
+        print(f"Against B: {option_b['name']}, a {option_b['description']}, from {option_b['country']}")
 
         user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+        
+        # Clearing screen
+        clear_screen()
 
-        # debugging
-        print(f"{option_a['name']}'s follower count: {option_a['follower_count']}")
-        print(f"{option_b['name']}'s follower count: {option_b['follower_count']}")
+        # Comparing value based on the required key, if their correct, increase score, if not, game over
+        is_correct = check_answer(user_choice, option_a['follower_count'], option_b['follower_count'])
 
-        # TODO: Compare value based on the required key, if their correct, increase score, if not, game over
-        if option_a['follower_count'] > option_b['follower_count']:
-            if user_choice == 'a':
-                score += 1
-                # To keep last option in option a
-                option_a = option_b
-                system('cls')
-                print(art.logo)
-                print(f"You're right! Current score: {score}.")
-            else:
-                system('cls')
-                print(art.logo)
-                print(f"Sorry, that's wrong. Final score: {score}.")
-                return
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
         else:
-            if user_choice == 'b':
-                score += 1
-                # To keep last option in option a
-                option_a = option_b
-                system('cls')
-                print(art.logo)
-                print(f"You're right! Current score: {score}.")
-            else:
-                system('cls')
-                print(art.logo)
-                print(f"Sorry, that's wrong. Final score: {score}.")
-                return
+            should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}.")
 
+        # Store dictionary of option_b in option_a
+        option_a = option_b
 
         # TODO: If correct, keep second option, make it the first option and pick a new option B using a new random number
         random_number_n = random.randint(0, data_size-1)
